02f6e11424895a5dc867519f45582c5ff59ae666
Bardhito26 <109962802+Bardhito26@users.noreply.github.com>
2022-07-25 14:14:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
-student1=18
-student2=19
-student3=22
-student4=23
-student5=17
-group=student1+student2+student3+student4+student5
-print(group/5)
+#Miniaventure
+emri=input('Shenoni emrin tuaj: ')
+print('Miresevini',emri,'ne kete aventur')
 
-print("Numri")
-x=int(input())
-y=x%2
-if y==0:
-    print("eshte cift")
-elif y!=0:
-    print("eshte tek")
+pergjigjja=input('Gjendeni ne nje udhekryq a doni te shkoni majtas apo djathtas?:')
 
-nba=["Golden State","Cleveland","San Antonio"]
-print(nba[0])
+if pergjigjja == 'majtas':
+    pergjigjja=input('Keni hasur ne nje perbindesh.Doni te luftoni apo ta shmangni perbindshin.Pergjigju luftim apo shmangje:')
 
-
-
-
-
-
-
-
-
-
-
+if pergjigjja =='luftim':
+    print('Ju luftuat furishem por vdiqet nga plaget qe morret nga perbindeshi!!!')
+elif pergjigjja =='shmangje':
+   pergjigjja=input('Ju u larguat me sukses nga perbindeshi.Tani keni hasur ne njeri te panjohur ai ju ka ofruar ushqim deshironi te pranoni apo jo?(Pergjigju po ose jo):')
+if pergjigjja == 'po':
+    print('Ju jeni helmuar nga ushqimi i ti dhe keni pasur nje vdekje tragjike.!!!')
+elif pergjigjja == 'jo':
+    print('Ti pe kurthin e njeriut dhe u largove.Pastaj u ktheve ne shtepi dhe jetove i lumtur jasht rreziqeve.')
+elif pergjigjja =='djathtas':
+    print('Eshte e bllokuar,kthehu mbrapa')
+else:
+    print('Pergjigjje jovalide')
