634676d6c242ce4ba070a900db4e81d6457c71a5
Bardhito26 <109962802+Bardhito26@users.noreply.github.com>
2022-07-25 14:12:23 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+student1=18
+student2=19
+student3=22
+student4=23
+student5=17
+group=student1+student2+student3+student4+student5
+print(group/5)
+
+print("Numri")
+x=int(input())
+y=x%2
+if y==0:
+    print("eshte cift")
+elif y!=0:
+    print("eshte tek")
+
+nba=["Golden State","Cleveland","San Antonio"]
+print(nba[0])
+
+
+
+
+
+
+
+
+
+
+
