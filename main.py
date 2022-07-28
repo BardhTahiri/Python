4b73bba3eb892bdbd36cdd97a67d701ce7c72ff3
Bardhito26 <109962802+Bardhito26@users.noreply.github.com>
2022-07-28 19:57:01 +0200

    Add files via upload
    Python Nba datascience

--- main.py
+++ main.py
@@ -1,21 +1,64 @@
-#Miniaventure
-emri=input('Shenoni emrin tuaj: ')
-print('Miresevini',emri,'ne kete aventur')
+import pandas as pd
+import matplotlib.pyplot as plt
+df=pd.read_csv('NBA_Team_Stats2.csv')
 
-pergjigjja=input('Gjendeni ne nje udhekryq a doni te shkoni majtas apo djathtas?:')
+print("Shkruani sezonen qe deshironi:")
+given_year = input()
+#Ne varesi nga inputi i sezones i grupojme top 5 ekipat sipas ati sezoni sipas pikeve
+filt=df.groupby("Year")
+topfive = filt.get_group(given_year).head(5)
 
-if pergjigjja == 'majtas':
-    pergjigjja=input('Keni hasur ne nje perbindesh.Doni te luftoni apo ta shmangni perbindshin.Pergjigju luftim apo shmangje:')
+x=list(topfive['Team'])
+y=list(topfive['Points'])
 
-if pergjigjja =='luftim':
-    print('Ju luftuat furishem por vdiqet nga plaget qe morret nga perbindeshi!!!')
-elif pergjigjja =='shmangje':
-   pergjigjja=input('Ju u larguat me sukses nga perbindeshi.Tani keni hasur ne njeri te panjohur ai ju ka ofruar ushqim deshironi te pranoni apo jo?(Pergjigju po ose jo):')
-if pergjigjja == 'po':
-    print('Ju jeni helmuar nga ushqimi i ti dhe keni pasur nje vdekje tragjike.!!!')
-elif pergjigjja == 'jo':
-    print('Ti pe kurthin e njeriut dhe u largove.Pastaj u ktheve ne shtepi dhe jetove i lumtur jasht rreziqeve.')
-elif pergjigjja =='djathtas':
-    print('Eshte e bllokuar,kthehu mbrapa')
-else:
-    print('Pergjigjje jovalide')
+#Ato informata i vendosim ne graf per qartim me te lehte
+ax=plt.axes()
+plt.title('Top 5 Ekipat sipas pikeve,sezoni ' + given_year)
+plt.xlabel('Team')
+plt.ylabel('Points')
+plt.bar(x,y,color='b',ls='-',lw=0.5,width=0.6)
+plt.show()
+
+#E formojne edhe nje graf per te treguar top 5 ekipat sipas ofenzives se pergjithshme
+PRO=df.sort_values(by='Offensive Overall',ascending=False).head(5)
+plt.title('Top 5 Ekipat sipas sulmit ,sezoni ' + given_year)
+plt.xlabel('Ekipa')
+plt.ylabel('Offensive Overall')
+plt.bar(x,y,color='b',ls='--',lw=0.5,width=0.6)
+ax.set_facecolor('Black')
+plt.show()
+
+#E formojme edhe nje graf per te treguar top 5 ekipat sipad defenzives se pergjithshme
+
+DRBS=df.sort_values(by='Defensive Overall',ascending=False).head(5)
+plt.title('Top 5 Ekipat sipas mbrojtjes , sezoni ' + given_year)
+plt.xlabel('')
+plt.ylabel('Deffensive Overall')
+plt.bar(x,y,color='b',ls='--',lw=0.5,width=0.6)
+plt.show()
+
+#Best 5 teams average Points per Game team from 1997-2022
+
+print('Best 5 teams average Points per Game team from 1997-2022')
+filt=df.sort_values(by='Points',ascending=False).head(5)
+print(filt)
+
+#Best defensive team
+
+print("Top 5 all time deffensive team")
+DRBS=df.sort_values(by='Defensive Overall',ascending=False).head(5)
+print(DRBS)
+
+#Permes inputit i marrim statistikat dhe ekipen dhe e paraqesim ne graf
+print('Shkruani ekipen:')
+x=input()
+print('Shkruani statistiken:')
+y=input()
+a=df[df['Team']==x]
+b=pd.DataFrame(a,columns=['Year',y])
+plt.plot(b['Year'],b[y],color='blue',marker='o')
+plt.title( x + " " + y + " " + 'from 1997-2022')
+plt.xlabel('Year')
+plt.ylabel(y)
+plt.grid(True)
+plt.show()
